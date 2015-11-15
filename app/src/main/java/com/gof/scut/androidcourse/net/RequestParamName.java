e9ce8d99bd064fabbfe784457799d3e0df9824a9
郭佳哲 <346672577@qq.com>
2015-11-15 10:52:38 +0800

    add some const request params

--- app/src/main/java/com/gof/scut/androidcourse/net/RequestParamName.java
+++ app/src/main/java/com/gof/scut/androidcourse/net/RequestParamName.java
@@ -5,9 +5,11 @@ package com.gof.scut.androidcourse.net;
  * Created by gjz on 11/2/15.
  */
 public class RequestParamName {
-    public static final String TOKEN = "token";
-    public static final String PHONE = "phone";
-    public static final String USERNAME = "username";
-    public static final String PASSWORD = "password";
-
+    public static final String TOKEN = "token";//用户token
+    public static final String USERNAME = "username";//登录时的用户名
+    public static final String PASSWORD = "password";//密码
+    public static final String PHONE = "phone";//电话号码
+    public static final String NAME = "name";//姓名
+    public static final String COMPANY = "company";//公司、单位
+    public static final String CARDID = "cardid";//名片id
 }
